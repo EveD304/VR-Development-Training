d2c532b2f38176a3dfa55083c1494d1a9c1fe5d3
EveD304 <deseve24@gmail.com>
2025-04-10 00:20:35 +0300

    clock testing

--- Assets/_Course Library/Scripts/Custom/ClockTime.cs
+++ Assets/_Course Library/Scripts/Custom/ClockTime.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 
 public class ClockTime : MonoBehaviour
 {
-    public GameObject hourHand;
-    public GameObject minuteHand;
-    public GameObject secondHand;
+    public Transform hourHand, minuteHand, secondHand;
+
+    const float degreesPerHour = 30f, degreesPerMinute = 6f, degreesPerSecond = 6f;
 
     string oldSeconds;
 
@@ -25,12 +25,18 @@ public class ClockTime : MonoBehaviour
 
     void UpdateTimer()
     {
-        int hoursInt = int.Parse(System.DateTime.UtcNow.ToLocalTime().ToString("hh"));
-        int minutesInt = int.Parse(System.DateTime.UtcNow.ToString("mm"));
-        int secondsInt = int.Parse(System.DateTime.UtcNow.ToString("ss"));
-        
-        print(hoursInt + ":" + minutesInt + ":" + secondsInt);
+        DateTime time = DateTime.Now;
 
-       // iTween.RotateTo(secondHand,iTween.Hash("z", secondsInt))
+        hourHand.localRotation = Quaternion.Euler(time.Hour * degreesPerHour, 0f, 0f);
+        minuteHand.localRotation = Quaternion.Euler(time.Minute * degreesPerMinute, 0f, 0f);
+        secondHand.localRotation = Quaternion.Euler(time.Second * degreesPerSecond, 0f, 0f);
+
+        //float hoursF = float.Parse(System.DateTime.UtcNow.ToLocalTime().ToString("hh"));
+        //float minutesF = float.Parse(System.DateTime.UtcNow.ToString("mm"));
+        //float secondsF = float.Parse(System.DateTime.UtcNow.ToString("ss"));
+
+        //print(hoursInt + ":" + minutesInt + ":" + secondsInt);
+
+        //iTween.RotateTo(secondHand, iTween.Hash("y", secondsInt * 6 * -1, "time", 1, "easetype", "easeOutQuint"));
     }
 }
