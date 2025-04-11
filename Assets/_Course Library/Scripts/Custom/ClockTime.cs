8dad547e4ff031e2fd56f616548cf9b2f5cb3d7c
EveD304 <deseve24@gmail.com>
2025-04-11 15:28:45 +0300

    clock solved

--- Assets/_Course Library/Scripts/Custom/ClockTime.cs
+++ Assets/_Course Library/Scripts/Custom/ClockTime.cs
@@ -16,20 +16,11 @@ public class ClockTime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        string seconds = System.DateTime.UtcNow.ToString("ss");
-
-        if (seconds != oldSeconds)
-        {
-            UpdateTimer();
-        }
-        oldSeconds = seconds;
-    }
-
-    void UpdateTimer()
-    {
         DateTime time = DateTime.Now;
 
-        float hourRotation = time.Hour * 30f;
+        int currentHour = time.Hour % 12;
+        float hourRotation = (currentHour + time.Minute / 60f) * 30f;
+        //float hourRotation = time.Hour * 30f;
         float minutesRotation = time.Minute * 6f;
         float secondsRotation = time.Second * 6f;
 
@@ -37,8 +28,8 @@ public class ClockTime : MonoBehaviour
         minuteHand.transform.localEulerAngles = new Vector3(0f, minutesRotation, 0f);
         secondsHand.transform.localEulerAngles = new Vector3(0f, secondsRotation, 0f);
 
-        Debug.Log(DateTime.Now.Hour);
-        Debug.Log(DateTime.Now.Minute);
-        Debug.Log(DateTime.Now.Second);
+        //Debug.Log(DateTime.Now.Hour);
+        //Debug.Log(DateTime.Now.Minute);
+        //Debug.Log(DateTime.Now.Second);
     }
 }
