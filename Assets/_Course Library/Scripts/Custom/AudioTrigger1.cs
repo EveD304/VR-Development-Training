379353855a08ba00c732ae458f87a87680820329
EveD304 <deseve24@gmail.com>
2025-05-02 21:26:51 +0300

    Audio Loop Fix

--- Assets/_Course Library/Scripts/Custom/AudioTrigger1.cs
+++ Assets/_Course Library/Scripts/Custom/AudioTrigger1.cs
@@ -8,6 +8,8 @@ public class AudioTrigger1 : MonoBehaviour
     Collider soundTrigger;
     public AudioClip[] birdSounds;
     //public int count = 0;
+    [Range(0f,8f)]
+    public float timeOffset = 0.5f;
     [Range(0.1f, 0.5f)]
     public float pitchChangeMultiplier = 0.2f;
 
@@ -19,15 +21,27 @@ public class AudioTrigger1 : MonoBehaviour
     
     private void OnTriggerEnter(Collider collider)
     {
-
-        source.clip = birdSounds[Random.Range(0, birdSounds.Length)];
-        source.pitch = Random.Range(1 - pitchChangeMultiplier, 1 + pitchChangeMultiplier);
-        source.Play();
-        //count++;
+        StartCoroutine(RepeatClips());
     }
 
     private void OnTriggerExit(Collider collider)
     {
         source.Stop();
+    }
+
+    private IEnumerator RepeatClips()
+    {
+        while (true)
+        {
+            foreach (AudioClip clip in birdSounds)
+            {
+                source.clip = birdSounds[Random.Range(0, birdSounds.Length)];
+                source.pitch = Random.Range(1 - pitchChangeMultiplier, 1 + pitchChangeMultiplier);
+                source.Play();
+
+                float duration = source.clip.length + timeOffset;
+                yield return new WaitForSeconds(duration);
+            }
+        }
     }
 }
